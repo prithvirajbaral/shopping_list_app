ec4463c9a2baec8f51456804c0ba6e7e3eeaf5d5
prithvirajbaral <prithvirajbaral@gmail.com>
2025-02-13 16:41:04 +0530

    save response in  a variable by using async and aawait

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -19,12 +19,12 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
       final url = Uri.https('flutter-prep-119b2-default-rtdb.firebaseio.com',
           '/shopping-list.json');
-      http.post(
+      final response = await http.post(
         url,
         headers: {
           'Content-Type': 'application/json',
@@ -37,6 +37,15 @@ class _NewItemState extends State<NewItem> {
           },
         ),
       );
+
+      print(response.body);
+      print(response.statusCode);
+
+      if (!context.mounted) {
+        return;
+      }
+      Navigator.of(context).pop();
+
       // Navigator.of(context).pop(
       //   GroceryItem(
       //     id: DateTime.now().toString(),
